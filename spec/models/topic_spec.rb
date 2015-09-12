ffaa954345473f021ed65b403fb7529025d6e1cd
Ken Posey <kennethwposey@yahoo.com>
2015-09-12 15:42:50 -0700

    assignment 35 validate topics

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -4,6 +4,11 @@ include RandomData
 RSpec.describe Topic, type: :model do
 #  pending "add some examples to (or delete) #{__FILE__}"
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+  it { should validate_length_of(:name).is_at_least(5) }
+  it { should validate_length_of(:description).is_at_least(15) }
   it { should have_many(:posts) }
 
   describe "attributes" do
